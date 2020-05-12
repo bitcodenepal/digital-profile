3b76a268141b027fbe09c6f19e89cc48a9832219
shashankj99 <shashankj677@gmail.com>
2020-05-12 23:26:50 +0545

    modified hospital section

--- digital_profile/app/Http/Controllers/Health/HospitalController.php
+++ digital_profile/app/Http/Controllers/Health/HospitalController.php
@@ -2,84 +2,87 @@
 
 namespace App\Http\Controllers\Health;
 
-use Illuminate\Http\Request;
+use App\Health\Hospital;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Health\HospitalRequest;
+use App\Services\NumberConverter;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
+
+/**
+ * class HospitalController
+ * 
+ * @package: App\Http\Controllers\Health
+ * @author: Shashank Jha <shashankj677@gmail.com>
+ */
 
 class HospitalController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
+    public function __construct() {
+        $this->middleware('auth');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+    public function index() {
+        return view('health.hospital.index')
+            ->with('hospitals', Hospital::all())
+            ->with('numberConverter', new NumberConverter);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+    public function create() {
+        return view('health.hospital.create');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+    public function store(HospitalRequest $request) {
+        try {
+            DB::beginTransaction();
+            Hospital::create($request->validated());
+            DB::commit();
+
+            Session::flash('success', 'विवरण सफलतापूर्वक थपियो');
+            return redirect()->route('hospital.index');
+        } catch (\Exception $error) {
+            DB::rollBack();
+            Session::flash('error', $error->getMessage());
+            return redirect()->back();
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+    public function show($id) {
+        Session::flash('info', "माफ गर्नुहोस्! अनुरोध गरिएको यूआरआई फेला पार्न असमर्थ");
+        return redirect()->back();
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
+    public function edit($id) {
+        $hospital = Hospital::findOrFail($id);
+        return view('health.hospital.edit')
+            ->with('hospital', $hospital);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+    public function update(HospitalRequest $request, $id) {
+        try {
+            $hospital = Hospital::findOrFail($id);
+
+            DB::beginTransaction();
+            $hospital->update($request->validated());
+            DB::commit();
+
+            Session::flash('success', "विवरण सफलतापूर्वक परिवर्तन गरियो");
+            return redirect()->route('literacy.index');
+
+        } catch (\Exception $error) {
+            DB::rollBack();
+            Session::flash('error', $error->getMessage());
+            return redirect()->back();
+        }
+    }
+
+    public function destroy($id) {
+        $hospital = Hospital::find($id);
+        if ($hospital) {
+            $hospital->delete();
+            return response($hospital->name. " को विवरण सफलतापूर्वक हटाइएको छ");
+        } else {
+            return response("डाटा हटाउन असमर्थ");
+        }
     }
 }
--- digital_profile/resources/views/health/hospital/index.blade.php
+++ digital_profile/resources/views/health/hospital/index.blade.php
@@ -1,1 +1,177 @@
-शैया
+@extends('layouts.app')
+
+@section('content-header')
+    <div class="row mt-3 mb-3">
+        <div class="col-12 col-sm-7 col-md-7">
+            <h1 class="text-dark">स्वास्थ संस्थाको विवरण तालिका</h1>
+        </div>
+        <div class="col-12 col-sm-5 col-md-5">
+            <a href="{{ route('hospital.create') }}" class="btn btn-md btn-info float-right" id="create-detail"><i class="fas fa-plus-circle fa-fw"></i> विवरण थप्नुहोस्</a>
+        </div>
+    </div>
+@endsection
+
+@section('content')
+    
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered hover table-sm">
+                            <thead class="text-center bg-gradient-danger">
+                                <tr>
+                                    <th rowspan="2">स्वास्थ संस्थाको नाम र ठेगाना</th>
+                                    <th rowspan="2">वडा नम्बर</th>
+                                    <th colspan="2">स्वास्थकर्मी</th>
+                                    <th rowspan="2">शैया संख्या</th>
+                                    <th colspan="13">उपलब्ध सेवा तथा सुविधा</th>
+                                    <th colspan="7">कार्यरत स्वास्थकर्मीको संख्या</th>
+                                    <th rowspan="2">कार्यहरु</th>
+                                </tr>
+                                <tr>
+                                    <th>स्वीकृत दरबन्दी</th>
+                                    <th>कार्यरत</th>
+                                    <th>प्रसुती सेवा</th>
+                                    <th>ल्याब</th>
+                                    <th>क्लिनिक</th>
+                                    <th>एक्सरे</th>
+                                    <th>परिवार नियोजन</th>
+                                    <th>खोप सेवा</th>
+                                    <th>सुरक्षित मातृत्व</th>
+                                    <th>पोषण सेवा</th>
+                                    <th>रक्त संचार</th>
+                                    <th>फार्मेसी</th>
+                                    <th>आँखा उपचार सेवा</th>
+                                    <th>स्वास्थ शिक्षण सेवा</th>
+                                    <th>परामर्श सेवा</th>
+                                    <th>विशेसज्ञ चिकित्सक</th>
+                                    <th>सामान्य चिकित्सक</th>
+                                    <th>हे.अ.</th>
+                                    <th>नर्स</th>
+                                    <th>अहेब</th>
+                                    <th>अनमी</th>
+                                    <th>ल्याब टेक्निसियन</th>
+                                </tr>
+                            </thead>
+                            <tbody class="text-center">
+                                @php
+                                    $position = $working = $bed = $specialist = $physician = $assistant = $nurse = $worker = $midwifery = $technician = 0;
+                                @endphp
+                                @foreach ($hospitals as $hospital)
+                                    <tr>
+                                        <td>{{ $hospital->name }}</td>
+                                        <td>{{ $numberConverter->devanagari($hospital->ward_no) }}</td>
+                                        <td>{{ $numberConverter->devanagari($hospital->position) }}</td>
+                                        <td>{{ $numberConverter->devanagari($hospital->working) }}</td>
+                                        <td>{{ $numberConverter->devanagari($hospital->bed) }}</td>
+                                        <td>{{ $hospital->maternity }}</td>
+                                        <td>{{ $hospital->lab }}</td>
+                                        <td>{{ $hospital->clinic }}</td>
+                                        <td>{{ $hospital->radiography }}</td>
+                                        <td>{{ $hospital->family_planning }}</td>
+                                        <td>{{ $hospital->vaccination }}</td>
+                                        <td>{{ $hospital->motherhood }}</td>
+                                        <td>{{ $hospital->nutrition }}</td>
+                                        <td>{{ $hospital->blood }}</td>
+                                        <td>{{ $hospital->pharmacy }}</td>
+                                        <td>{{ $hospital->optometry }}</td>
+                                        <td>{{ $hospital->health_education }}</td>
+                                        <td>{{ $hospital->consultation }}</td>
+                                        <td>{{ $numberConverter->devanagari($hospital->specialist) }}</td>
+                                        <td>{{ $numberConverter->devanagari($hospital->physician) }}</td>
+                                        <td>{{ $numberConverter->devanagari($hospital->assistant) }}</td>
+                                        <td>{{ $numberConverter->devanagari($hospital->nurse) }}</td>
+                                        <td>{{ $numberConverter->devanagari($hospital->worker) }}</td>
+                                        <td>{{ $numberConverter->devanagari($hospital->midwifery) }}</td>
+                                        <td>{{ $numberConverter->devanagari($hospital->technician) }}</td>
+                                        <td>
+                                            <a href="{{ route('hospital.edit', $hospital->id) }}" class="btn btn-xs btn-primary edit-detail" title="सम्पादन गर्नुहोस्"><i class="fas fa-edit"></i></a>
+                                            <button class="btn btn-xs btn-danger delete-detail" title="हटाउनुहोस्" data-id={{ $hospital->id }}><i class="fas fa-trash"></i></button>
+                                        </td>
+                                    </tr>
+                                    @php
+                                        $position += $hospital->position;
+                                        $working += $hospital->working;
+                                        $bed += $hospital->bed;
+                                        $specialist += $hospital->specialist;
+                                        $physician += $hospital->physician;
+                                        $assistant += $hospital->assistant;
+                                        $nurse += $hospital->nurse;
+                                        $worker += $hospital->worker;
+                                        $midwifery += $hospital->midwifery;
+                                        $technician += $hospital->technician;
+                                    @endphp
+                                @endforeach
+                            </tbody>
+                            <tfoot class="text-center bg-gradient-secondary">
+                                <tr>
+                                    <td></td>
+                                    <td>जम्मा</td>
+                                    <td>{{ $numberConverter->devanagari($position) }}</td>
+                                    <td>{{ $numberConverter->devanagari($working) }}</td>
+                                    <td>{{ $numberConverter->devanagari($bed) }}</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td>{{ $numberConverter->devanagari($specialist) }}</td>
+                                    <td>{{ $numberConverter->devanagari($physician) }}</td>
+                                    <td>{{ $numberConverter->devanagari($assistant) }}</td>
+                                    <td>{{ $numberConverter->devanagari($nurse) }}</td>
+                                    <td>{{ $numberConverter->devanagari($worker) }}</td>
+                                    <td>{{ $numberConverter->devanagari($midwifery) }}</td>
+                                    <td>{{ $numberConverter->devanagari($technician) }}</td>
+                                    <td></td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
+
+@section('custom-scripts')
+
+    <script>
+
+        jQuery(function($) {
+            $(".delete-detail").click(function() {
+                if (confirm("के तपाईं यो विवरण निश्चय हटाउन चाहानुहुन्छ?")) {
+                    let id = this.dataset.id,
+                        url = "{{ route('hospital.destroy', ':id') }}";
+
+                    url = url.replace(":id", id);
+
+                    $.ajax({
+                        url: url,
+                        type: "DELETE",
+                        data: {
+                            "_token": "{{ csrf_token() }}",
+                        },
+                        success:function(response) {
+                            alert(response);
+                            location.reload();
+                        }
+                    });
+                } else {
+                    return false;
+                }
+            });
+        });
+
+    </script>
+
+@endsection
